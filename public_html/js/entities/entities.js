23dcbdf08cae92c14d4c7131c13abd0658b8d78c
lazarus  morris <lazarusmorris@ymail.com>
2014-12-09 13:59:57 -0800

    adding mushroom

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -17,18 +17,18 @@ game.PlayerEntity = me.Entity.extend({
         //create animation called smallwalk using pictures of tye image defined above(mario)
         //sets the animation to run through 8-13
         ///the last number says we switch between pictures every 80 milliseconds
-        
-        this.renderable.addAnimation("smallWalk", [8,9,10,11,12,13],80); 
+
+        this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
 
         this.renderable.setCurrentAnimation("idle");
         //sets the speed we go on the x axis(first number) and y axis(second number)
         this.body.setVelocity(5, 20);
         //sets the camera(viewport) to follow maroi's position(pos) on both the x and y axis//
         me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
-        
+
     },
     update: function(delta) {
-       //checks if the right key is pressed and if it is, executes the following statement
+        //checks if the right key is pressed and if it is, executes the following statement
 
         if (me.input.isKeyPressed("right")) {
             //sets the positiion of mario on the x axis by adding the x value from the setVelocity times the timer.tick
@@ -40,15 +40,15 @@ game.PlayerEntity = me.Entity.extend({
         } else {
             this.body.vel.x = 0;
         }
-                this.body.update(delta);
-                me.collision.check(this, true, this.collideHandler.bind(this), true);
+        this.body.update(delta);
+        me.collision.check(this, true, this.collideHandler.bind(this), true);
 
 
         if (me.input.isKeyPressed("up")) {
             this.body.vel.y -= this.body.accel.y * me.timer.tick;
         }
 
-       
+
         me.collision.check(this, true, this.collideHandler.bind(this), true);
 
         if (this.body.vel.x !== 0) {
@@ -59,26 +59,26 @@ game.PlayerEntity = me.Entity.extend({
         } else {
             this.renderable.setCurrentAnimation("idle");
         }
-        
-        
+
+
 
         this._super(me.Entity, "update", [delta]);
         return true;
     },
     collideHandler: function(response) {
-        var ydif = this.pos.y -response.b.pos.y;
-          console.log(ydif);
-        
-      if(response.b.type === 'badguy'){
-          if(ydif <= -115){
-             response.b.alive = false; 
-          }else{
-              me.state.change(me.state.MENU);
-            } 
-          
-          
-   
-      }
+        var ydif = this.pos.y - response.b.pos.y;
+        console.log(ydif);
+
+        if (response.b.type === 'badguy') {
+            if (ydif <= -115) {
+                response.b.alive = false;
+            } else {
+                me.state.change(me.state.MENU);
+            }
+
+
+
+        }
     }
 
 });
@@ -94,7 +94,7 @@ game.LevelTrigger = me.Entity.extend({
         this.level = settings.level;
         this.xSpawn = settings.xSpawn;
         this.ySpawn = settings.ySpawn;
-        
+
     },
     onCollision: function() {
         console.log("collision");
@@ -106,8 +106,8 @@ game.LevelTrigger = me.Entity.extend({
 
 });
 game.BadGuy = me.Entity.extend({
-    init:  function(x,y,settings){
-               this._super(me.Entity, 'init', [x, y, {
+    init: function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, {
                 image: "slime",
                 spritewidth: "60",
                 spiteheight: "28",
@@ -122,46 +122,60 @@ game.BadGuy = me.Entity.extend({
         x = this.pos.x;
         this.startX = x;
         this.endX = x + width - this.spritewidth;
-        this.pos.x = x +width - this .spritewidth;
+        this.pos.x = x + width - this.spritewidth;
         this.updateBounds();
-        
+
         this.alwaysUpdate = true;
         this.walkLeft = false;
         this.alive = true;
-        this.type="badguy";
-        
+        this.type = "badguy";
+
         //this.renderable.addAnimation("run",[0,1,2],80);
         //this.renderable.setCurrentAnimation("run");
-      
-        this.body.setVelocity(4,6);
- 
+
+        this.body.setVelocity(4, 6);
+
     },
-    update: function(delta){
+    update: function(delta) {
         this.body.update(delta);
-        me.collision.check(this, true, this.collisionHandler.bind(this),true);
-        
-        if(this.alive){
-             if(this.walkLeft && this.pos.x <= this.startX){
-                 this.walkLeft = false;
-                 
-                 
-                 
-             }else if(!this.walkLeft && this.pos.x >= this.endX){
-                 this.walkLeft = true;
-             }
-             this.flipX(!this.walkLeft);
-             this.body.vel.x += (this.walkLeft) ? -this.body.accel.x * me.timer.tick:this.body.accel.x * me.timer.tick;
-        }else{
+        me.collision.check(this, true, this.collisionHandler.bind(this), true);
+
+        if (this.alive) {
+            if (this.walkLeft && this.pos.x <= this.startX) {
+                this.walkLeft = false;
+
+
+
+            } else if (!this.walkLeft && this.pos.x >= this.endX) {
+                this.walkLeft = true;
+            }
+            this.flipX(!this.walkLeft);
+            this.body.vel.x += (this.walkLeft) ? -this.body.accel.x * me.timer.tick : this.body.accel.x * me.timer.tick;
+        } else {
             me.game.world.removeChild(this);
         }
         this._super(me.Entity, "update", [delta]);
         return true;
-        
-        
+
+
     },
-    
-    collisionHandler: function(){
-        
+    collisionHandler: function() {
+
     }
-}); 
-//game.Mushroom
+});
+game.Mushroom = me.Entity.extend({
+    init: function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, {
+                image: "mushroom",
+                spritewidth: "64",
+                spiteheight: "64",
+                width: 64,
+                height: 64,
+                getShape: function() {
+                    return (new me.Rect(0, 0, 64, 64)).toPolygon();
+                }
+            }]);
+        me.collision.check(this);
+        this.type = "mushroom";
+    }
+});
